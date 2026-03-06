0dee2bf88c7ca506f7aeb7f36d3a5f6b42aff112
Matheus Lima <matheeus161@gmail.com>
2026-03-06 08:38:03 -0300

    config: teste container with mysql

--- src/test/java/com/eliasnogueira/paymentservice/integration/PaymentIntegrationTest.java
+++ src/test/java/com/eliasnogueira/paymentservice/integration/PaymentIntegrationTest.java
@@ -13,7 +13,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -33,6 +35,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Testcontainers
+@ActiveProfiles("mysql")
 class PaymentIntegrationTest {
 
     @Autowired
